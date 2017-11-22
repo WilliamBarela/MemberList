c2caca86b3336146649d08e7473017743d378d16
William Barela <william.barela.dev@gmail.com>
2017-11-21 19:24:43 -0600

    add json fetch request

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,25 @@
 //script.js
+
+(function(){
+    const peopleJsonUrl = "https://raw.githubusercontent.com/WilliamBarela/MemberList/master/app/assets/json/people.json";
+
+    fetch(peopleJsonUrl).then(function(response) {
+			return(response.json());
+		}).then(function(response) {
+			updateUISuccess(response);
+		}).catch(function() {
+			updateUIFailure();
+		});
+
+    function updateUISuccess(response) {
+      const config = response.config;
+      const staff = response.staff;
+
+      console.log(config);
+      console.log(staff);
+    };
+
+    function updateUIFailure(){
+      console.log("Request failed.");
+    };
+})();
