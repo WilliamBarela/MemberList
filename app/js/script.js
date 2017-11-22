199573b35a8b4f8c2f08f37da1fbe08c8532cd3e
WilliamBarela <william.barela.dev@gmail.com>
2017-11-22 00:17:26 -0600

    add div row structure

--- app/js/script.js
+++ app/js/script.js
@@ -12,11 +12,36 @@
 		});
 
     function updateUISuccess(response) {
+      const people = document.querySelector('#people')
       const config = response.config;
       const staff = response.staff;
 
-      console.log(config);
-      console.log(staff);
+      let row = document.createElement('div');
+      let img_div = document.createElement('div');
+      let info_div = document.createElement('div');
+
+      genPersonRow();
+
+      function genImgDiv(){
+
+      };
+
+      function genInfoDiv(){
+
+      };
+
+      function joinAttachRow(){
+        row.appendChild(img_div);
+        row.appendChild(info_div);
+        console.log(row);
+        people.appendChild(row);
+      };
+
+      function genPersonRow(){
+        genImgDiv();
+        genInfoDiv();
+        joinAttachRow();
+      };
     };
 
     function updateUIFailure(){
